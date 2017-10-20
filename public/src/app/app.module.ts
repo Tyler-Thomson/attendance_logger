d79c6ee8e08037ecb200a224004fe030de0e2637
Tyler Thomson <tylerthomson@protonmail.com>
2017-10-20 17:14:20 -0500

    Removed git submodule from /public

---
+++ public/src/app/app.module.ts
@@ -0,0 +1,29 @@
+// Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+
+// Components
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+
+// Services
+import { LoginService } from './login.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [LoginService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
