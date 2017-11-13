23046299b8a81a30cfe359e65edce99866b7af88
Tyler Thomson <tylerthomson@protonmail.com>
2017-11-12 22:33:47 -0600

    Added attendance model

--- server/models/attendance.js
+++ server/models/attendance.js
@@ -1,9 +1,9 @@
 let mongoose = require('mongoose');
 
 let AttendanceSchema = new mongoose.Schema({
-    'user': { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     'date': { type: Date },
-    'time': { type: String }
+    'time': { type: String },
+    'user': { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 }, { timestamps: true });
 
 mongoose.model('Attendance', AttendanceSchema);
